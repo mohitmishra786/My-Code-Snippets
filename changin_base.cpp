b54232047e5c71de05f3fa03ad1551bbe50f9822
DuKe786 <71754779+mohitmishra786@users.noreply.github.com>
2021-10-05 09:59:16 +0530

    added chaning_base.cpp

---
+++ changin_base.cpp
@@ -0,0 +1,95 @@
+//░░░░░░░░░░ // You have struggled your way here with no guidance from anyone,keep the faith
+//░░░░░░░░▄▐░▄▄█████▄▄     //You have practiced a hell lot of questions,have faith in yourself
+//░░░░░░▄█████████████▄▀▄▄▄▄▄    //Dont just stare at the problem use that damn pen and paper
+//░░░░░█████████████████▄██████ // write down whats in ur head !
+//░░░░████▐█████▌████████▌█████▌
+//░░░████▌█████▌█░████████▐▀██▀
+//░▄█████░█████▌░█░▀██████▌█▄▄▀▄
+//░▌███▌█░▐███▌▌░░▄▄░▌█▌███▐███░▀
+//▐░▐██░░▄▄▐▀█░░░▐▄█▀▌█▐███▐█
+//░░███░▌▄█▌░░▀░░▀██░░▀██████▌
+//░░░▀█▌▀██▀░▄░░░░░░░░░███▐███
+//░░░░██▌░░░░░░░░░░░░░▐███████▌
+//░░░░███░░░░░▀█▀░░░░░▐██▐███▀▌
+//░░░░▌█▌█▄░░░░░░░░░▄▄████▀░▀    
+//░░░░░░█▀██▄▄▄░▄▄▀▀▒█▀█░▀
+//░░░░░░░░░▀░▀█▀▌▒▒▒░▐▄▄
+//░░░░░░░░▄▄▀▀▄░░░░░░▄▀░▀▀▄▄
+//░░░░░░▄▀░▄▀▄░▌░░░▄▀░░░░░░▄▀▀▄
+//░░░░░▐▒▄▀▄▀░▌▀▄▄▀░░░░░░▄▀▒▒▒▐
+//░░░░▐▒▒▌▀▄░░░░░▌░░░░▄▄▀▒▐▒▒▒▒▌
+//░░░▐▒▒▐░▌░▀▄░░▄▀▀▄▀▀▒▌▒▐▒▒▒▒▐▐
+//░░░▌▄▀░░░▄▀░█▀▒▒▒▒▀▄▒▌▐▒▒▒▒▒▌▌
+//░░▄▀▒▐░▄▀░░░▌▒▐▒▐▒▒▒▌▀▒▒▒▒▒▐▒▌
+
+//AFTER STRUGGLE,PEACE RESIDES,WORK HARD TODAY FOR THE BETTER TOMORROW !
+
+/**
+ *    author:  immadmohit      
+**/
+
+#include <bits/stdc++.h>
+using namespace std;
+#define endl "\n"
+#define ff first
+#define ss second
+#define vec vector
+#define pb push_back
+#define ll long long
+#define vi vector<int>
+#define vll vector<ll>
+#define pll pair<ll,ll>
+#define pi pair<int,int>
+#define mod(a,b) (a%b+b)%b
+#define ceil(a,b) ((a+b-1)/b)
+#define flushout cout.flush();
+#define sz(x) ((int) (x).size())
+#define __lcm(a,b) (a*b)/__gcd(a,b)
+#define all(x) (x).begin(), (x).end()
+#define vin(a) for(auto &ii : a)cin>>ii
+#define f(i,a,b) for(int i=(a);i<(b);i++)
+#define fr(i,a,b) for(int i=(b-1);i>=(a);i--)
+#define dbg(x) cout<<'>'<< #x <<':'<< x <<endl;
+#define pout(x,n) cout<<fixed<<setprecision(x)<<n
+#define vout(v) for(auto ii:v)cout<<ii<<' ';cout<<endl;
+const int M = 1000000007;
+
+/* CODE STARTS HERE */
+
+void init_code(){
+    #ifndef Mohit_Mishra
+        freopen("Input.txt", "r", stdin);
+        freopen("Output.txt", "w", stdout);
+        freopen("Error.txt", "w", stderr);
+    #endif
+}
+
+ll base_convreter(ll  n , int base){
+	ll base10 = n;
+	ll base_x = 0;
+	ll pos = 1;
+	while(base10 > 0){
+		base_x += pos * (base10 % base);
+		base10/=9;
+		pos *= 10;
+	}
+	return base_x;
+}
+void solve(){
+    //Why it has to be me
+    
+    
+}
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);cout.tie(0);
+    init_code();
+
+    int t;
+    cin >> t;
+    while(t--){
+        solve();
+    }
+
+}
