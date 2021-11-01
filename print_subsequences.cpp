787a93e423347793aad81fd70774b373130ea81c
DuKe786 <71754779+mohitmishra786@users.noreply.github.com>
2021-11-01 11:10:05 +0530

    Create print_subsequences.cpp

---
+++ print_subsequences.cpp
@@ -0,0 +1,17 @@
+void subs(string s , string o){
+    if(s.length() == 0){
+        cout << o << endl;
+        return;
+    }
+
+    subs(s.substr(1) , o);
+    subs(s.substr(1) , o + s[0]);
+
+}
+void solve(){
+    //Why it has to be me
+    string input;
+    cin >> input;
+    string output = "";
+    subs(input , output);
+}
